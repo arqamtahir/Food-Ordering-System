eeda5375e82c9f0ecae2ec1307fb0f5240b06b75
arqamtahir <muhammadarqam920@gmail.com>
2022-08-25 16:45:41 +0500

    Add admin-charges model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_114131) do
+ActiveRecord::Schema.define(version: 2022_08_25_114508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2022_08_25_114131) do
     t.string "description"
     t.float "price"
     t.integer "post_status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "admin_charges", force: :cascade do |t|
+    t.float "amount"
+    t.datetime "pay_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
