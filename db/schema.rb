0265bf1a63c4201b9ebdefb4bdfed4a02a054a5f
arqamtahir <muhammadarqam920@gmail.com>
2022-09-02 15:39:11 +0500

    Remove email field from customer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_02_064136) do
+ActiveRecord::Schema.define(version: 2022_09_02_103700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 2022_09_02_064136) do
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "email"
     t.string "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
