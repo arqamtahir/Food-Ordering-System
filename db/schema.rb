0becdc6e20b8166bd7fb4519493415609b98e064
arqamtahir <muhammadarqam920@gmail.com>
2022-09-02 15:40:54 +0500

    Remove email field from employee

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_02_103700) do
+ActiveRecord::Schema.define(version: 2022_09_02_104012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,7 +118,6 @@ ActiveRecord::Schema.define(version: 2022_09_02_103700) do
   create_table "employees", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "email"
     t.string "phone"
     t.string "designation"
     t.datetime "created_at", precision: 6, null: false
