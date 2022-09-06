e8dfba272f452a9dbde944886fe3f0c74d4f5eb6
arqamtahir <muhammadarqam920@gmail.com>
2022-09-06 12:30:49 +0500

    Add indexes to admin resturant's resource

--- app/admin/resturants.rb
+++ app/admin/resturants.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Resturant do
   permit_params :name, :location, :contact, :open_close_status
+
+  index do
+    selectable_column
+    column :id
+    column :name
+    column :location
+    column :open_close_status
+    column :contact
+    actions
+  end
 end
