2bdcfed429b8a9173579f1628c181929c6cd8cf9
arqamtahir <muhammadarqam920@gmail.com>
2022-09-06 17:14:58 +0500

    Resolve issue of allow null resturant's references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_06_115308) do
+ActiveRecord::Schema.define(version: 2022_09_06_121151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -180,7 +180,7 @@ ActiveRecord::Schema.define(version: 2022_09_06_115308) do
     t.string "invited_by_type"
     t.bigint "invited_by_id"
     t.integer "invitations_count", default: 0
-    t.bigint "resturant_id", null: false
+    t.bigint "resturant_id"
     t.index ["email"], name: "index_employees_on_email", unique: true
     t.index ["invitation_token"], name: "index_employees_on_invitation_token", unique: true
     t.index ["invited_by_id"], name: "index_employees_on_invited_by_id"
