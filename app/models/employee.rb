55b25a37842594d6974e16479b1526b476396453
arqamtahir <muhammadarqam920@gmail.com>
2022-09-06 18:20:49 +0500

    Add resturants reference optional true in employees

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,7 +3,7 @@ class Employee < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :resturant
+  belongs_to :resturant ,optional:true
   has_many :order
   has_many :subordinates, class_name: 'Employee',foreign_key: "manager_id"
   belongs_to :manager, class_name: 'Employee', optional: true
