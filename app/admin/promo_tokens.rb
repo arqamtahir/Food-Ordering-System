da4a6f86beae9b224d43cd4b6e8af1b83063d1f7
arqamtahir <muhammadarqam920@gmail.com>
2022-09-07 11:17:54 +0500

    Permit params in admin/promo_token

--- app/admin/promo_tokens.rb
+++ app/admin/promo_tokens.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register PromoToken do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :token_name, :discount_percentage, :post_status
+  permit_params :token_name, :discount_percentage, :post_status
   #
   # or
   #
