75d3b3d62bf8ebe0fccb4b77ae68cef373145d35
arqamtahir <muhammadarqam920@gmail.com>
2022-09-07 11:19:23 +0500

    Add cut_off model  as active_admin resource

---
+++ app/admin/cut_offs.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register CutOff do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :percentage
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:percentage]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
