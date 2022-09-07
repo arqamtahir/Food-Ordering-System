04b1aa9eceffd11d4b34d073e85289d2f7b84ad6
arqamtahir <muhammadarqam920@gmail.com>
2022-09-07 11:22:44 +0500

    Permit params in admin/admin_charge

--- app/admin/admin_charges.rb
+++ app/admin/admin_charges.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register AdminCharge do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :amount, :pay_date, :order_id
+  permit_params :amount, :pay_date, :order_id
   #
   # or
   #
