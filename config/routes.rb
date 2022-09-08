320bc555a201499e2a446c6e98e3026b3c35dc1a
arqamtahir <muhammadarqam920@gmail.com>
2022-09-08 15:48:50 +0500

    Add addons as resources in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :employees, controllers: { invitations: 'employees/invitations' }
   devise_for :customers
+  resources :addons
   root 'home#index'
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
