2c9d196a3469d42144f77fcd42cddcb7b6d1dab1
arqamtahir <muhammadarqam920@gmail.com>
2022-09-08 17:08:33 +0500

    Update show action in addon's controller

--- app/controllers/addons_controller.rb
+++ app/controllers/addons_controller.rb
@@ -1,10 +1,11 @@
 class AddonsController < ApplicationController
 
     def index
-       @addons=Addon.all
+      @addons=Addon.all
     end
 
     def show
+			@addon=Addon.find(params[:id])
     end
 
     def new
