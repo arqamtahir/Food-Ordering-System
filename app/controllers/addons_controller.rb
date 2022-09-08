0ea2d4eae6cbfd05b8ce76b686fbed16544c3c02
arqamtahir <muhammadarqam920@gmail.com>
2022-09-08 16:27:11 +0500

    Add edit,create,update & destroy  actions in addon's controller

--- app/controllers/addons_controller.rb
+++ app/controllers/addons_controller.rb
@@ -9,5 +9,18 @@ class AddonsController < ApplicationController
 
     def new
     end
+
+    def edit
+    end
+
+    def create
+
+    end
+
+    def update
+    end
+
+    def destroy
+    end
     
 end
