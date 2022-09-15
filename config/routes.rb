d040eb60c2aa9703ba4af0b80ab64cf85c3bd863
arqamtahir <muhammadarqam920@gmail.com>
2022-09-15 14:58:46 +0500

    Add resources for menus in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :employees, controllers: { invitations: 'employees/invitations' }
   devise_for :customers
   resources :addons
+  resources :menus
   root 'home#index'
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
