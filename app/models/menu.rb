fff8f7344651bf2c5e4c61688ac91cef9093eeef
arqamtahir <muhammadarqam920@gmail.com>
2022-09-15 13:42:27 +0500

    Add reference of join-table for has_and_belongs_to_many association in menu's model

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,5 +2,5 @@ class Menu < ApplicationRecord
     belongs_to :resturant
     has_and_belongs_to_many :timings
     has_and_belongs_to_many :menu_items
-    has_and_belongs_to_many :addons
+    has_and_belongs_to_many :addons , join_table: "addons_menus"
 end
