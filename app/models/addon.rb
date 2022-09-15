a53a901ac1283cf2c559de97f62e6573faa0ba9e
arqamtahir <muhammadarqam920@gmail.com>
2022-09-15 13:41:50 +0500

    Add reference of join-table for has_and_belongs_to_many association in addon's model

--- app/models/addon.rb
+++ app/models/addon.rb
@@ -1,4 +1,4 @@
 class Addon < ApplicationRecord
     enum post_status: { available: 1,  unavailable: 2 }
-    has_and_belongs_to_many :menus
+    has_and_belongs_to_many :menus , join_table: "addons_menus"
 end
