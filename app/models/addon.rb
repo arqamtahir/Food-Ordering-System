80db00c9e6b9b1ea0076111b819b93d4491e5ff0
arqamtahir <muhammadarqam920@gmail.com>
2022-09-15 14:38:14 +0500

    Add validations in addon's model

--- app/models/addon.rb
+++ app/models/addon.rb
@@ -1,4 +1,9 @@
 class Addon < ApplicationRecord
+    validates :name, presence: true , length: {minimum:,4 maximum:50}
+    validates :description, presence: true , length: {minimum:,6 maximum:100}
+    validates :price, presence: true 
+    validates :post_status, presence: true 
+
     enum post_status: { available: 1,  unavailable: 2 }
     has_and_belongs_to_many :menus , join_table: "addons_menus"
 end
