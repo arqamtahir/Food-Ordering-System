c7fea6853ffa168b04f4f73fd897bdfe2a0e1edc
arqamtahir <muhammadarqam920@gmail.com>
2022-09-16 10:48:13 +0500

    Remove authenticate_inviter method from application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   # Authenticate_invitor method
-    protected
+    # protected
 
-    def authenticate_inviter!
-      authenticate_admin_user!(force: true)
-    end
+    # def authenticate_inviter!
+    #   authenticate_admin_user!(force: true)
+    # end
 
 end
