825ed6bed78b293aa6c85166309b6868c5376ffe
arqamtahir <muhammadarqam920@gmail.com>
2022-09-16 11:40:11 +0500

    Update all actions in menu's controller

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,22 +1,60 @@
 class MenusController < ApplicationController
+  before_action :set_menu, only: [:show, :edit, :update, :destroy]
+  before_action :permit_params, only: [:update, :create]
+
   def index
+    @menus=Menu.all
   end
 
   def show
   end
 
   def new
-  end
-
-  def create
+    @menu=Menu.new
   end
 
   def edit
   end
 
+  def create
+    @menu=Menu.new(permit_params)
+    if @menu.save
+      flash[:notice] = "menu created successfully"
+      redirect_to menus_path
+    else
+      flash[:alert] = "Therer is some issue menu not created"
+      render :action => 'new'
+    end
+
+  end
+
   def update
+    if @menu.update(permit_params)
+      flash[:notice] = "menu updated successfully"
+      redirect_to menus_path(@menu)
+    else
+      flash[:alert] = "Therer is some issue menu not updated"
+      render :action => "update"
+    end
   end
 
   def destroy
+    if @menu.destroy
+      flash[:notice] = "menu deleted successfully"
+      redirect_to menus_path
+    else
+      flash[:alert] = "Therer is some issue menu not deleted"
+      render :action => "index"
+    end
+  end
+
+  private
+
+  def set_menu
+    @menu=Menu.find(params[:id])
+  end
+
+  def permit_params
+    params.require(:menu).permit(:name, addon_ids: [])
   end
 end
