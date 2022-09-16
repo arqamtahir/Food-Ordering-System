87afa423e6580a699d3c9141d0716337fd8ac114
arqamtahir <muhammadarqam920@gmail.com>
2022-09-16 18:13:36 +0500

    Remove devise_registrable from employee's model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,7 @@
 class Employee < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable,
          :recoverable, :rememberable, :validatable
   belongs_to :resturant ,optional:true
   has_many :order
