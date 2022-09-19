860424a8ca3a8c642f9244bd2cc2e8d82b40af1b
arqamtahir <muhammadarqam920@gmail.com>
2022-09-19 18:04:59 +0500

    Add resturants and addons associations

--- app/models/addon.rb
+++ app/models/addon.rb
@@ -6,4 +6,5 @@ class Addon < ApplicationRecord
 
     enum post_status: { available: 1,  unavailable: 2 }
     has_and_belongs_to_many :menus , join_table: "addons_menus"
+    belongs_to :resturant
 end
