43389ef73ba08187f528236d3fdd49e59648d22b
arqamtahir <muhammadarqam920@gmail.com>
2022-09-20 13:08:45 +0500

    Add resources for menu_items in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :customers
   resources :addons
   resources :menus
+  resources :menu_items
   root 'home#index'
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
