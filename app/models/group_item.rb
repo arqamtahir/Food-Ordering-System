3604c66f39f9b96b18193614561879095091300a
arqamtahir <muhammadarqam920@gmail.com>
2022-10-05 13:32:33 +0500

    Add food_item association in group_item's model

--- app/models/group_item.rb
+++ app/models/group_item.rb
@@ -4,6 +4,7 @@ class GroupItem < ApplicationRecord
     has_many :options , inverse_of: :group_item, dependent: :destroy
     has_many :deal_items,as: :deal_itemable
     has_many :menu_items,as: :menu_itemable
+    has_many :food_items
     accepts_nested_attributes_for :options, reject_if: :all_blank, allow_destroy: true
 
     enum post_status: { available: 1,  unavailable: 2 }
