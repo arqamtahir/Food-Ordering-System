25aa4a1f9c3d367488e7b80b46f073d821d892db
arqamtahir <muhammadarqam920@gmail.com>
2022-10-05 13:30:24 +0500

    Modify addon's model for ransackable_scope filteration

--- app/models/addon.rb
+++ app/models/addon.rb
@@ -1,7 +1,7 @@
 class Addon < ApplicationRecord
     included Availability
     enum post_status: { available: 1,  unavailable: 2 }
-    
+
     has_and_belongs_to_many :menu_items , join_table: "addons_menu_items"
     belongs_to :resturant
 
@@ -9,4 +9,8 @@ class Addon < ApplicationRecord
     validates :description, presence: true , length: {minimum:6, maximum:100}
     validates :price, presence: true 
     validates :post_status, presence: true 
+
+    def self.ransackable_scopes(auth_object = nil)
+	    %i(available unavailable)
+    end
 end
