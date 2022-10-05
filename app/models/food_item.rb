f6b9ed8e7e7d3fe5bded2cd03158b2c6679b5fb9
arqamtahir <muhammadarqam920@gmail.com>
2022-10-05 13:31:20 +0500

    Modify food_item's model for ransackable_scope filteration

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -7,5 +7,9 @@ class FoodItem < ApplicationRecord
     has_and_belongs_to_many :discounts
     has_many :deal_items,as: :deal_itemable
     has_many :order_items,as: :order_itemable
+    belongs_to :group_item
     
+    def self.ransackable_scopes(auth_object = nil)
+	    %i(available unavailable)
+    end
 end
