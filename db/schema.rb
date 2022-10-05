f5ac3217fffaebdf795bd905ed6c1e89623f4a0c
arqamtahir <muhammadarqam920@gmail.com>
2022-10-05 16:00:00 +0500

    Generate migration to add discarded_at to addons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_29_052516) do
+ActiveRecord::Schema.define(version: 2022_10_05_105736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2022_09_29_052516) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "resturant_id"
+    t.datetime "discarded_at"
+    t.index ["discarded_at"], name: "index_addons_on_discarded_at"
     t.index ["resturant_id"], name: "index_addons_on_resturant_id"
   end
 
