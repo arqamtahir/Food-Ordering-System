dfa0e2d669efc2ce16739228ee511102f668738d
arqamtahir <muhammadarqam920@gmail.com>
2022-10-06 11:28:47 +0500

    Add nested routes for discarded and restore under food_items

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,21 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :employees, controllers: { invitations: 'employees/invitations' }
   devise_for :customers
-  resources :food_items
+
+  resources :food_items do
+    get :discarded , on: :collection 
+    get :restore , on: :member 
+  end
+
   resources :addons do 
     get :discarded , on: :collection 
     get :restore , on: :member 
   end
+
   resources :group_items , shallow: true do
     resources :options 
   end
+  
   resources :menus
   resources :menu_items
   root 'home#index'
