939184b0f68959149758a5ddb929b7b7a0ffb1e1
arqamtahir <muhammadarqam920@gmail.com>
2022-10-11 15:10:43 +0500

    Add discarded_at field to food_items

---
+++ db/migrate/20221006064515_add_discarded_at_to_fod_items.rb
@@ -0,0 +1,6 @@
+class AddDiscardedAtToFodItems < ActiveRecord::Migration[6.1]
+  def change
+    add_column :food_items, :discarded_at, :datetime
+    add_index :food_items, :discarded_at
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_05_105736) do
+ActiveRecord::Schema.define(version: 2022_10_06_064515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -201,6 +201,8 @@ ActiveRecord::Schema.define(version: 2022_10_05_105736) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "group_item_id", null: false
+    t.datetime "discarded_at"
+    t.index ["discarded_at"], name: "index_food_items_on_discarded_at"
     t.index ["group_item_id"], name: "index_food_items_on_group_item_id"
   end
 
