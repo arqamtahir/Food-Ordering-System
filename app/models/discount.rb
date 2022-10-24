2b1adc9d3ee2619017a0bf4284aca27135b44531
arqamtahir <muhammadarqam920@gmail.com>
2022-10-24 18:09:15 +0500

    Include availability concern and make enum in discounts model

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,5 +1,7 @@
 class Discount < ApplicationRecord
+    included Availability
     enum post_status: { available: 1,  unavailable: 2 }
+    enum discount_type: { flat: 1,  percentage: 2 }
 
     has_and_belongs_to_many :food_items
     has_many :discount_timings
