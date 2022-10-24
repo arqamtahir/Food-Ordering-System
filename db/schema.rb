ce6bc47040f2cea37308681cf2ace2b2d8c8caae
arqamtahir <muhammadarqam920@gmail.com>
2022-10-24 19:13:08 +0500

    Add discarded_at column in discount table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_24_101426) do
+ActiveRecord::Schema.define(version: 2022_10_24_141150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -155,6 +155,8 @@ ActiveRecord::Schema.define(version: 2022_10_24_101426) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.datetime "discarded_at"
+    t.index ["discarded_at"], name: "index_discounts_on_discarded_at"
   end
 
   create_table "discounts_food_items", id: false, force: :cascade do |t|
