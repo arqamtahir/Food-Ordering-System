2ade438c95b442fc46b3855de531f48fc01d3e63
arqamtahir <muhammadarqam920@gmail.com>
2022-10-26 13:07:00 +0500

    Allow discount_model to accept nested attributes for discount_timings

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -6,7 +6,8 @@ class Discount < ApplicationRecord
     enum discount_type: { flat: 1,  percentage: 2 }
 
     has_and_belongs_to_many :food_items
-    has_many :discount_timings
+    has_many :discount_timings , dependent: :destroy
+    accepts_nested_attributes_for :discount_timings, reject_if: :all_blank, allow_destroy: true
 
     def self.ransackable_scopes(auth_object = nil)
 	    %i(available unavailable)
