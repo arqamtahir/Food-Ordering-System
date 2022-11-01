bb50911dab70e88feac33fad32adf4e4de2118c3
arqamtahir <muhammadarqam920@gmail.com>
2022-11-01 12:34:39 +0500

    Add assocciation related menu_items in menu's model

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,5 +3,8 @@ class Menu < ApplicationRecord
     
     belongs_to :resturant, optional: true
     has_and_belongs_to_many :timings
-    has_and_belongs_to_many :menu_items
+    has_many :menu_items, dependent: :destroy
+
+    accepts_nested_attributes_for :menu_items, allow_destroy: true
+
 end
