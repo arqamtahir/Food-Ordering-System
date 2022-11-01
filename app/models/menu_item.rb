883241b269085baadc984574028810489c0985e6
arqamtahir <muhammadarqam920@gmail.com>
2022-11-01 12:35:33 +0500

    Add assocciation related menus in menu_item's model

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -2,8 +2,8 @@ class MenuItem < ApplicationRecord
     included Availability
     enum post_status: { available: 1,  unavailable: 2 }
 
-    has_and_belongs_to_many :menus
     belongs_to :menu_itemable , polymorphic: true
     has_and_belongs_to_many :addons , join_table: "addons_menu_items"
+    belongs_to :menu, optional: true
 end
 
