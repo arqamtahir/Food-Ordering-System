874d7d1945576e59a077c1255ae0953663b50ab6
arqamtahir <muhammadarqam920@gmail.com>
2022-11-09 14:56:03 +0500

    Add devise registerable in employee's model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -7,7 +7,7 @@ class Employee < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :registerable
   belongs_to :resturant ,optional:true
   has_many :order
   has_many :subordinates, class_name: 'Employee',foreign_key: "manager_id"
