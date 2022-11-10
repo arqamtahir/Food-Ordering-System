f6ac5552961c19195144aed32343ad32b85d0694
arqamtahir <muhammadarqam920@gmail.com>
2022-11-10 12:09:07 +0500

    Add validation for employees

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -13,6 +13,19 @@ class Employee < ApplicationRecord
   has_many :subordinates, class_name: 'Employee',foreign_key: "manager_id"
   belongs_to :manager, class_name: 'Employee', optional: true
 
+  validates :first_name, :last_name, :phone, :email, :resturant, presence: true
+  validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*\z/ }
+  validates :first_name, :last_name, length: { minimum: 2, maximum: 20 }
+  validates :email, uniqueness: true, on: :create,
+            format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :phone, numericality:{
+                    only_integer: true,
+                    message: 'can only contain numbers'},
+                    length:{
+                    minimum: 11,
+                    maximum: 16,
+                    message: 'containing invalid length'}
+
   def full_name
     "#{first_name} #{last_name}"
   end
