af63d9385580725928c0323e4ef1a8e3b9621161
arqamtahir <muhammadarqam920@gmail.com>
2022-11-10 15:29:00 +0500

    Add discarded_at field in menus

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_10_074400) do
+ActiveRecord::Schema.define(version: 2022_11_10_102430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -255,6 +255,8 @@ ActiveRecord::Schema.define(version: 2022_11_10_074400) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "resturant_id", null: false
+    t.datetime "discarded_at"
+    t.index ["discarded_at"], name: "index_menus_on_discarded_at"
     t.index ["resturant_id"], name: "index_menus_on_resturant_id"
   end
 
