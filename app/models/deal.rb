6c3e0a459846bdcc51f68c701879489f1b26df8a
arqamtahir <muhammadarqam920@gmail.com>
2022-11-11 17:53:09 +0500

    Modified deal_items assocciation in deal model and accept nested attributes for it

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,7 +1,8 @@
 class Deal < ApplicationRecord
     enum post_status: { available: 1,  unavailable: 2 }
 
-    has_and_belongs_to_many :deal_items
+    has_many :deal_items, dependent: :destroy
     has_many :menu_items,as: :menu_itemable
 
+    accepts_nested_attributes_for :deal_items, allow_destroy: true
 end
