0900221e97a51252974cb4b4ede5c7c74a2c34c6
arqamtahir <muhammadarqam920@gmail.com>
2022-11-11 17:53:46 +0500

    Modified deals assocciation in deal_item model

--- app/models/deal_item.rb
+++ app/models/deal_item.rb
@@ -1,7 +1,7 @@
 class DealItem < ApplicationRecord
     enum post_status: { available: 1,  unavailable: 2 }
     
-    has_and_belongs_to_many :deals
+    belongs_to :deals
     belongs_to :deal_itemable, polymorphic: true
     has_many :order_items,as: :order_itemable
 
