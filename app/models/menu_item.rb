798bb28ce7f68c1138a43fdf399ca577dfe34bc1
arqamtahir <muhammadarqam920@gmail.com>
2022-11-18 12:05:44 +0500

    Modify menu_item model to remove addons association

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -3,7 +3,6 @@ class MenuItem < ApplicationRecord
     enum post_status: { available: 1,  unavailable: 2 }
 
     belongs_to :menu_itemable , polymorphic: true
-    has_and_belongs_to_many :addons , join_table: "addons_menu_items"
     belongs_to :menu, optional: true
 end
 
