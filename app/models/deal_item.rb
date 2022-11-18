e0da9a441741527e60a20f37b0761868af2a3387
arqamtahir <muhammadarqam920@gmail.com>
2022-11-18 11:57:26 +0500

    Modify deal_items model to make deal association optional true

--- app/models/deal_item.rb
+++ app/models/deal_item.rb
@@ -1,7 +1,7 @@
 class DealItem < ApplicationRecord
     enum post_status: { available: 1,  unavailable: 2 }
     
-    belongs_to :deals
+    belongs_to :deals, optional: true
     belongs_to :deal_itemable, polymorphic: true
     has_many :order_items,as: :order_itemable
 
