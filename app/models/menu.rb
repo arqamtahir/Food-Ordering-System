580220e41b0bc71563835cc3f140a26f313a16e0
arqamtahir <muhammadarqam920@gmail.com>
2022-11-24 18:28:11 +0500

    Add timings association and accept nested attributes for it in menu's model

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -5,12 +5,15 @@ class Menu < ApplicationRecord
     enum post_status: {available: 1, unavailable: 2}
 
     validates :name, presence: true , length: {minimum:4, maximum:50}
-    
+
     belongs_to :resturant, optional: true
     has_and_belongs_to_many :timings
     has_many :menu_items, dependent: :destroy
+    has_many :timings, dependent: :destroy
+
 
     accepts_nested_attributes_for :menu_items, allow_destroy: true
+    accepts_nested_attributes_for :timings, reject_if: :all_blank, allow_destroy: true
 
     def self.ransackable_scopes(auth_object = nil)
 	    %i(available unavailable)
